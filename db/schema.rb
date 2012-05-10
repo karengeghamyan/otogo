952d11de1f07e0212ec7c933bafa9edd8df5eae0
karengeghamyan <karen.geghamyan@gmail.com>
2012-05-10 17:32:46 +0500

    cleanup 2

--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,5 @@
 
 ActiveRecord::Schema.define(:version => 20120509155733) do
 
-  create_table "news", :force => true do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "test2s", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
